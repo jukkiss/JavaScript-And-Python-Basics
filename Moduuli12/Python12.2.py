2f3942931b889b197b40110150df8118cbc8ec2d
jukkiss <savela.j@gmail.com>
2023-10-30 12:34:08 +0200

    Moduuli 12, tehtävä 12.2

---
+++ Moduuli12/Python12.2.py
@@ -0,0 +1,22 @@
+import requests
+def kelvinmuunnin(kelvin):
+    return kelvin - 273.15
+
+
+avain = "468e0269450b7facca54cac35b5b3b3c"
+kaupunki = input("Anna kaupungin nimi: ")
+pyyntö = f"http://api.openweathermap.org/data/2.5/weather?q={kaupunki}&appid={avain}"
+
+try:
+    vastaus = requests.get(pyyntö)
+    json_vastaus = vastaus.json()
+
+    if vastaus.status_code==200:
+        sää = json_vastaus["weather"][0]["description"]
+        kelvin_asteet = json_vastaus["main"]["temp"]
+        celsius_asteet = kelvinmuunnin(kelvin_asteet)
+        print(F"Paikkakunta: {kaupunki}. Sään kuvaus: {sää}. Lämpötila {celsius_asteet:.2f} C")
+except requests.exceptions.RequestException as e:
+    print("Haku epäonnistui")
+
+
