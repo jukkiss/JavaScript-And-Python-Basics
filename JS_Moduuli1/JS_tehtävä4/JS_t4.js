7a0ffae734665824180722b22360f255245d1587
jukkiss <savela.j@gmail.com>
2023-11-20 12:27:44 +0200

    Javascript Moduuli 1 tehtävä 4 muutettu tehtävänannon mukaiseksi

--- JS_Moduuli1/JS_tehtävä4/JS_t4.js
+++ JS_Moduuli1/JS_tehtävä4/JS_t4.js
@@ -1,9 +1,27 @@
 'use strict'
 
+
 const inputti = prompt("Anna nimesi: ");
 
 const ryhmat = ['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw'];
-const randomRyhma = ryhmat[Math.floor(Math.random() * ryhmat.length)];
+const randomNumero = Math.floor(Math.random() * 4) + 1;
+
+let randomRyhma;
+switch (randomNumero) {
+  case 1:
+    randomRyhma = ryhmat[0];
+    break;
+  case 2:
+    randomRyhma = ryhmat[1];
+    break;
+  case 3:
+    randomRyhma = ryhmat[2]
+    break;
+  case 4:
+    randomRyhma = ryhmat[3]
+    break;
+}
+
 
 const elementti = document.querySelector('#kohde')
 
